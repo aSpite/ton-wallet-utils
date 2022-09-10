e728e687b95dd28f92f587e25a5b83cc2f0cb258
Yablonev <y@blonev.art>
2022-09-10 08:29:48 +0100

    Update getTransactions.js

--- services/getTransactions.js
+++ services/getTransactions.js
@@ -6,7 +6,7 @@ export async function getTransactions({ address }) {
   const mc = await client.getMasterchainInfoExt()
   const block = mc.last
 
-  const state = await client.getAccountState(Address.parse(address), block.id, 5000)
+  const state = await client.getAccountState(Address.parse(address), block)
   const transactions = await client.getAccountTransactions(Address.parse(address), state.lastTx?.lt, state.lastTx?.hash, 10)
 
   return transactions || []
