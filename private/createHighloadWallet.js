d8b981460368dead918f5247b5fd5971c33a1504
Yablonev <y@blonev.art>
2022-10-15 16:46:08 +0100

    !

--- private/createHighloadWallet.js
+++ private/createHighloadWallet.js
@@ -7,7 +7,8 @@ export async function createHighloadWallet() {
   const { publicKey, secretKey } = keyPairFromSeed(seed)
   const wallet = new Wallets.ContractHighloadWalletV2(0, publicKey, 1)
   const address = wallet.address.toString('base64', { bounceable: true, urlSafe: true, workchain: 0 })
+  const addressNonBouncable = wallet.address.toString('base64', { bounceable: false, urlSafe: true, workchain: 0 })
   console.log(`Please, deposit some TON to your new wallet ${address}`)
   seed = seed.toString('hex')
-  return { address, seed, publicKey, secretKey }
+  return { address, addressNonBouncable, seed, publicKey, secretKey }
 }
--- services/loadWallet.js
+++ services/loadWallet.js
@@ -1,12 +1,25 @@
 import { VERSION_TYPES } from '../private/config.js'
 import { tonMnemonic, tonweb } from '../private/tonweb.js'
+import { keyPairFromSeed } from 'ton-crypto'
+import { Wallets } from 'ton3-contracts'
 
-export async function loadWallet({ version = 'v4R2', mnemonic }) {
-  if (version !== VERSION_TYPES.v4R2) return { error: 'Unknown version' }
+export async function loadWallet({ version = 'v4R2', mnemonic = [], seed = '' }) {
+  if (version === VERSION_TYPES.highload) return await loadHighloadWallet({ seed })
+  if (version !== VERSION_TYPES.v4R2) return { error: 'Unknown version. Please use v4R2 or highload' }
   const keyPair = await tonMnemonic.mnemonicToKeyPair(mnemonic)
   const { publicKey, secretKey } = keyPair
   const wallet = tonweb.wallet.create({ publicKey })
   const addr = await wallet.getAddress()
   const address = addr.toString(true, true, true, false)
-  return { address, mnemonic, publicKey, secretKey }
+  const addressNonBouncable = addr.toString(true, true, false, false)
+  return { address, addressNonBouncable, mnemonic, publicKey, secretKey }
+}
+
+export async function loadHighloadWallet({ seed }) {
+  const seedBuffer = Buffer.from(seed, 'hex')
+  const { publicKey, secretKey } = keyPairFromSeed(seedBuffer)
+  const wallet = new Wallets.ContractHighloadWalletV2(0, publicKey, 1)
+  const address = wallet.address.toString('base64', { bounceable: true, urlSafe: true, workchain: 0 })
+  const addressNonBouncable = wallet.address.toString('base64', { bounceable: false, urlSafe: true, workchain: 0 })
+  return { address, addressNonBouncable, publicKey, secretKey }
 }
