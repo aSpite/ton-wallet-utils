3fb8ed289b26b47d5bc648e92d88715ed96117b6
Yablonev <y@blonev.art>
2023-01-28 10:37:32 +0000

    Update getJettons.js

--- _modules/getJettons.js
+++ _modules/getJettons.js
@@ -5,8 +5,10 @@ export async function getJettons({ address }) {
   const rawAddress = flipAddressType(address)
 
   try {
+    const token = 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsidGcwOUY5Il0sImV4cCI6MTgxNjIwMjE3NCwiaXNzIjoiQHRvbmFwaV9ib3QiLCJqdGkiOiI1VTU1VjdVVEUzQk1OMzdYVk1EWVYzSUYiLCJzY29wZSI6InNlcnZlciIsInN1YiI6InRvbmFwaSJ9.tDG3J0wdqIYs1P227m2YeaMk_18CrOw7mE1TYBQ7wVTCCz9WVdx7OI4zX9bL7EvlzdrEzWOG9qma1RpC4V_oCQ'
+
     const url = `https://tonapi.io/v1/jetton/getBalances?account=${rawAddress}`
-    const { statusText, data } = await axios.get(url)
+    const { statusText, data } = await axios.get(url, { headers: { 'Authorization': `Bearer ${token}` } })
     if (statusText === 'OK') {
       let jettons = []
 
@@ -15,7 +17,9 @@ export async function getJettons({ address }) {
           tokenBalance.balance = +tokenBalance.balance
 
           const url = `https://tonapi.io/v1/jetton/getInfo?account=${tokenBalance.jetton_address}`
-          const { statusText, data } = await axios.get(url)
+
+          const { statusText, data } = await axios.get(url, { headers: { 'Authorization': `Bearer ${token}` } })
+
           const tokenBalanceWithInfo = { ...tokenBalance }
           if (statusText === 'OK') {
             tokenBalanceWithInfo.jetton_info = data
@@ -32,6 +36,7 @@ export async function getJettons({ address }) {
           if (!jetton.jetton_address || !jetton.wallet_address?.address) return jetton
           jetton.jetton_address = flipAddressType(jetton.jetton_address)
           jetton.wallet_address.address = flipAddressType(jetton.wallet_address.address )
+          jetton.jetton_info.metadata.image = jetton.jetton_info?.metadata?.image?.replace('ipfs://', IPFS_GATEWAY)
           return jetton
         })
 
@@ -39,7 +44,7 @@ export async function getJettons({ address }) {
       }
     }
   } catch(e) {
-    console.log(e.code)
+    console.log(e)
     return []
   }
 }
